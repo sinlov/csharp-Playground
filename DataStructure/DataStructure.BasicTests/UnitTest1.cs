d99392c3df1e27ed04d5bad8b9e4108588f8e367
sinlov <sinlovppt@gmail.com>
2021-10-18 14:45:30 +0800

    feat: add DataStructure Tire for less module to test

--- DataStructure/DataStructure.BasicTests/UnitTest1.cs
+++ DataStructure/DataStructure.BasicTests/UnitTest1.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Reflection;
-using DataStructure.BasicTests;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace DataStructure
+namespace DataStructure.BasicTests
 {
     public class UnitTest1 : BaseTests
     {
         [Fact]
         public void Test1()
         {
-            TLog("test UnitTest1");
         }
 
         public UnitTest1(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
---
+++ DataStructure/DataStructure.KTrie.TestBenchmark/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using BenchmarkDotNet.Running;
+using Xunit;
+
+namespace DataStructure.KTrie.TestBenchmark
+{
+    public class Program
+    {
+        // static void Main()
+        // {
+        //     
+        // }
+    }
+}

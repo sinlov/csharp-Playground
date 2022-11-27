129a0873db23f69b6ca7033fad10668b089575b3
sinlov <sinlovgmppt@gmail.com>
2022-11-27 20:02:14 +0800

    feat: try new project tree as dotnet 8.0

---
+++ NunitPlayground/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace NunitPlayground;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
